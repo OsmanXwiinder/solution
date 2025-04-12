9339cb074bd2fa793f73646a31b7cdbf20a31b7f
OsmanXwiinder <osmansaifi30@gmail.com>
2025-04-12 11:49:20 +0530

    error

--- Task-Tracker/middleware/user.js
+++ Task-Tracker/middleware/user.js
@@ -1,15 +1,24 @@
 const jwt = require('jsonwebtoken')
-require('dotenv').config();
 
+require('dotenv').config()
 const JWT_SECRET = process.env.JWT_SECRET;
-
-console.log(JWT_SECRET);
+console.log(JWT_SECRET)
 
 function usermiddleware(req,res,next){
     const username = req.headers.username;
     const tokenValue = req.headers.authorization;
 
+
+     // Check if token exists and starts with 'Bearer '
+
+    if (!tokenValue || !tokenValue.startsWith("Bearer ")) {
+        return res.status(401).json({
+            msg: "Authorization header missing or malformed"
+        });
+    }
+
     const token = tokenValue.split(" ")[1]
+    console.log(token)
 
     try{
         const decoded = jwt.verify(token,JWT_SECRET)
